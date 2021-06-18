df954d72baf230b862a63549bd461b04d88a20e9
Shoaib547 <84625981+Shoaib547@users.noreply.github.com>
2021-06-18 14:31:21 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,4 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 float average(int arr[],int len){
+  int sum = 0;
+  float avg;
+  for(int i = 0; i < len; i++){
+    sum += arr[];
+  }
+  avg = (float)sum/len;
+  return avg;
+}
   
